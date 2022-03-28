c6fe96405a0fb4ed35c77b8bec445ee6a9e6ce54
elifoksas <62424342+elifoksas@users.noreply.github.com>
2022-03-28 21:21:32 +0300

    firstApp

---
+++ 1.firstApp/1.firstApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  1.firstApp
+//
+//  Created by elif okşaş on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        myLabel.text = "Turkey"
+        
+        
+    }
+}
+
