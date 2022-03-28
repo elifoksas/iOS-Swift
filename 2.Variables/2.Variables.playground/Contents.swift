8803ebea9b60123b25e35897ebc5f0cb558b7dbb
elifoksas <62424342+elifoksas@users.noreply.github.com>
2022-03-28 21:44:46 +0300

    Variables

---
+++ 2.Variables/2.Variables.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+
+var myName = "Elif"
+var mySurname = "Oksas"
+print(myName)
+
+myName = "elif"
+print(myName)
+
+
+var internettenYeniGelenVeri = 50 //camelCase
+var internetten_yeni_gelen_veri = 50 //snake_case
+
+
+let myNumber = 50 //sabit, sonradan değiştirilemez.
+print(myNumber)
+
+let birinciNumaram=80
+let ikinciNumaram=12
+birinciNumaram/ikinciNumaram
+
+//double
+let pi=3.141529
+
+let doubleNumaram = 80.0
+let ikinciDoubleNumaram = 12.0
+doubleNumaram/ikinciDoubleNumaram
+
+//boolean
+var benimBooleanim = true
+benimBooleanim = false
+
+
+
+var benimSayim : Int32 = 50
+
+let benimYeniDouble : Float = 3.14
+
+//defining
+let myString : String
+
+//initilaziation
+myString = "elif"
+
+
+
+var myNewNumber : Int
+myNewNumber = 20
+let benimStringNumaram : String
+benimStringNumaram = String(myNewNumber)
+benimStringNumaram + "10"
+
+
+let stringNo = "400"
+let integerString = Int(stringNo)
+integerString! + 20
+
+
+
