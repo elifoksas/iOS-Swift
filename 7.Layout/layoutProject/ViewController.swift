2098c666dccd6557417977525ba44d9dca113a34
elifoksas <62424342+elifoksas@users.noreply.github.com>
2022-03-31 15:53:40 +0300

    Layout

--- 7.Layout/layoutProject/ViewController.swift
+++ 7.Layout/layoutProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     var BenimDegiskenim = 4
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
